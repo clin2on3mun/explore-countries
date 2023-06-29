6138ffafbea246e37eb3c21ba12651681da72acb
Munana Clinton <munanaclinton@gmail.com>
2023-06-29 14:34:52 +0200

    Add index.js, index.css and App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { fetchCountries } from './redux/country/countrySlice';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Showcountry from './components/CountryInfo';
+
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/country/:countryCode" element={<Showcountry />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
